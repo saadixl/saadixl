da604baab5993b9e3ba886bfcc354604663df5b5
A.M. Masudul Haque <A.M. Masudul Haque>
2016-04-30 01:22:30 +0600

    Nav, header, about done

---
+++ static/assets/js/saadixl.js
@@ -0,0 +1,30 @@
+/*
+	Nav Smooth Scrolling
+*/
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+
+$(document).ready(function(){
+
+    /* Back to top */
+    $(".navbar-brand").click(function(){
+        $('html, body').animate({
+          scrollTop: 0
+        }, 1000);
+    });
+
+
+});
